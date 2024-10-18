af60b217cf0bdf84ced0603a8a6262da8cc3deb4
Dhinesh kumaran S <dhineshkumaran2004@outlook.com>
2024-10-18 13:27:06 +0530

    Feat:Changes in components

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 function Card({ Crop, Description, Areas, imageURL }) {
     return (
-        <div className="w-[250px] h-auto bg-white p-4 rounded-lg shadow-md m-10">
+        <div className="w-[250px] h-auto bg-white p-4 rounded-lg shadow-md m-10 hover:translate-y-[-10px]">
             <img src={imageURL} alt={Crop} className="w-full h-48 object-cover rounded-t-lg" />
             <div className="p-4">
                 <h1 className="text-xl font-bold mb-2">{Crop}</h1>
